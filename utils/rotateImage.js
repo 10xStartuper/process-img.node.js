768076d1a853be205cc224955b03137b1020be94
Mukhammadyusuf Abdurakhimov <icoderx@yandex.com>
2022-04-25 15:55:29 +0300

    update

--- utils/rotateImage.js
+++ utils/rotateImage.js
@@ -2,10 +2,10 @@ const sharp = require("sharp");
 
 async function rotateImage() {
   try {
-    await sharp("../images/sammy.png")
+    await sharp("./images/sammy.png")
       .rotate(33, { background: { r: 0, g: 0, b: 0, alpha: 0 } })
       .blur(12)
-      .toFile("../output/sammy-rotated.png");
+      .toFile("./output/sammy-rotated.png");
   } catch (error) {
     console.log(error);
   }
