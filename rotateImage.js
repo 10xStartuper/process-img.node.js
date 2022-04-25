9755d3cab4e90f628604046cf7b9b4b1c617d0a6
Mukhammadyusuf Abdurakhimov <icoderx@yandex.com>
2022-04-25 15:44:09 +0300

    go

--- rotateImage.js
+++ rotateImage.js
@@ -4,6 +4,7 @@ async function rotateImage() {
   try {
     await sharp("./images/sammy.png")
       .rotate(33, { background: { r: 0, g: 0, b: 0, alpha: 0 } })
+      .blur(12)
       .toFile("./output/sammy-rotated.png");
   } catch (error) {
     console.log(error);
