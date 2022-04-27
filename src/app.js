7b6661949e261b02d5dd69ea0fbc20849616dd52
Mukhammadyusuf Abdurakhimov <icoderx@yandex.com>
2022-04-28 04:31:11 +0500

    update

--- src/app.js
+++ src/app.js
@@ -1,18 +1,25 @@
 const addTextOnImage = require("./addTextOnImage");
 const readXlsx = require("./read_xlsx");
 
-const main = async  () =>{
-    const users = await readXlsx();
-    users.forEach(async(data)=>{
-        const user = {
-            first_name: data[0],
-            last_name: data[1],
-            id: data[2]
-        }
-        const image = await addTextOnImage(user);
-    })
-}
+const main = async () => {
+  const users = await readXlsx();
+  let user = {};
+  await users.forEach(async (data) => {
+    user = {
+      first_name: data[1],
+      last_name: data[2],
+      id: data[0],
+    };
+    // regioun: data[3],
+    // date: new Date(data[4]),
+    // smena: data[5],
+    // building: data[6],
+    // group: data[7],
+    // seat_number: data[8],
+    await addTextOnImage(user);
+  });
+};
 
-main().catch(err=>{
-    console.log(err);
-})
+main().catch((err) => {
+  console.log(err);
+});
