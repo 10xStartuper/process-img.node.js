4c708ef74e09a605cb25f363e8431be8b605aa23
Mukhammadyusuf Abdurakhimov <icoderx@yandex.com>
2022-04-28 05:42:45 +0500

    App has been optimized

--- src/addTextOnImage.js
+++ src/addTextOnImage.js
@@ -10,27 +10,30 @@ async function addTextOnImage(user) {
       <style>
       .title { fill: #001; font-size: 18px; font-weight: 600;}
       </style>
-      <text x="267" y="378" text-anchor="start" class="title">${user.first_name}</text>
-      <text x="267" y="404" text-anchor="start" class="title">${user.last_name}</text>
-      <text x="267" y="430" text-anchor="start" class="title">${'11/01/2022'}</text>
+      <text x="267" y="378" text-anchor="start" class="title">${
+        user.first_name
+      }</text>
+      <text x="267" y="404" text-anchor="start" class="title">${
+        user.last_name
+      }</text>
+      <text x="267" y="430" text-anchor="start" class="title">${"11/01/2022"}</text>
     </svg>
     `;
     const svgBuffer = Buffer.from(svgImage);
     const barCodeBuffer = await generateBarCode(user.id);
-    const image = await sharp("./images/page.jpg")
-      .composite([
-        {
-          input: svgBuffer,
-          top: 0,
-          left: 0,
-        },
-        {
-            input: barCodeBuffer,
-            top: 18,
-            left: 32,
-        },
-      ])
-      .toFile("./results/"+ user.id +".png");
+    const image = await sharp("./images/page.jpg").composite([
+      {
+        input: svgBuffer,
+        top: 0,
+        left: 0,
+      },
+      {
+        input: barCodeBuffer,
+        top: 18,
+        left: 32,
+      },
+    ]);
+    await image.toFile("./results/" + user.id + ".png");
   } catch (error) {
     console.log(error);
   }
--- src/app.js
+++ src/app.js
@@ -1,25 +1,49 @@
+const cliProgress = require("cli-progress");
+const colors = require("ansi-colors");
 const addTextOnImage = require("./addTextOnImage");
 const readXlsx = require("./read_xlsx");
+const progressBar = new cliProgress.SingleBar(
+  {
+    format:
+      "Rendering |" +
+      colors.cyan("{bar}") +
+      "| {percentage}% || {value}/{total}",
+    barCompleteChar: "\u2588",
+    barIncompleteChar: "\u2591",
+    barsize: 42,
+    hideCursor: true,
+  },
+  cliProgress.Presets.shades_classic
+);
 
 const main = async () => {
   const users = await readXlsx();
+  progressBar.start(users.length, 0);
   let user = {};
-  await users.forEach(async (data) => {
+  let i = 0;
+  for (const data of users) {
     user = {
       first_name: data[1],
       last_name: data[2],
       id: data[0],
+      building: data[6],
     };
-    // regioun: data[3],
-    // date: new Date(data[4]),
-    // smena: data[5],
-    // building: data[6],
-    // group: data[7],
-    // seat_number: data[8],
     await addTextOnImage(user);
-  });
+
+    progressBar.increment();
+    progressBar.update(i);
+    i++;
+  }
+  progressBar.stop();
 };
 
 main().catch((err) => {
   console.log(err);
 });
+
+// regioun: data[3],
+// date: new Date(data[4]),
+// smena: data[5],
+// building: data[6],
+// group: data[7],
+// seat_number: data[8],
